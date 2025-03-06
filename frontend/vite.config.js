b9db0141821f72450a179126ebad798f35274335
Erickson De Souza Martinez <erickson.martinez@meta.com.br>
2025-03-05 22:59:25 -0300

    feat: config project

--- frontend/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-    define: {
-        'process.env': {
-            API: JSON.stringify(process.env.VITE_API_URL)
-        }
-    }
-});
