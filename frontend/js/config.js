31f8181b47d520ab0ba6633860bc4157bfbea400
Erickson De Souza Martinez <erickson.martinez@meta.com.br>
2025-04-24 00:06:43 -0300

    feat: config

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,14 +1,15 @@
+//frontend/js/config.js
 const API_URL = true ? "https://stok-5ytv.onrender.com" : "http://192.168.1.67:3000";
 
 const menuItems = [
     { name: "Home", route: "./home.html" },
     { name: "Financeiro", route: "./expense.html" },
-    { name: "Estoque", route: "./stock.html" },
-    { name: "Atividade", route: "./activity.html" },
-    { name: "Mercados", route: "./markets.html" },
-    { name: "Lista de Compras", route: "./shopping.html" },
-    { name: "Livros", route: "./book.html" },
-    { name: "Planning livros", route: "./planning.html" },
+    // { name: "Estoque", route: "./stock.html" },
+    // { name: "Atividade", route: "./activity.html" },
+    // { name: "Mercados", route: "./markets.html" },
+    // { name: "Lista de Compras", route: "./shopping.html" },
+    // { name: "Livros", route: "./book.html" },
+    // { name: "Planning livros", route: "./planning.html" },
     { name: "Meditação", route: "./meditation.html" },
 ];
 
